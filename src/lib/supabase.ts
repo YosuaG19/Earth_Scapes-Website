442ad688b97dac560e90d06a85c989f3529856da
YosuaG19 <yosuawg19@gmail.com>
2026-01-22 20:52:31 +0700

    Fix : server error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,27 +1,9 @@
-// // src/lib/supabase.ts
-// import { createBrowserClient } from '@supabase/ssr'
+// src/lib/supabase.ts
+import { createBrowserClient } from '@supabase/ssr'
 
-<<<<<<< HEAD
-// export function createClient() {
-//   return createBrowserClient(
-//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-//   )
-// }
-
-// // Usage in client components:
-// import { createClient } from '@/lib/supabase'
-
-// export default function ClientComponent() {
-//   const supabase = createClient(){
-    
-//   }
-// }
-=======
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
->>>>>>> a1126337d28a64364913bf711e65455f17b63c62
